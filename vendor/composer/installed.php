85d7c6260de3e4584f9d7a5c9ce01feb19c7f6ae
Maxopenstudio <71130450+Maxopenstudio@users.noreply.github.com>
2022-10-01 14:08:43 +0300

    Add initial set of files

---
+++ vendor/composer/installed.php
@@ -0,0 +1,50 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'v5.2.99',
+        'version' => '5.2.99.0',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => NULL,
+        'name' => 'symfony/website-skeleton',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'symfony/flex' => array(
+            'pretty_version' => 'v1.19.3',
+            'version' => '1.19.3.0',
+            'type' => 'composer-plugin',
+            'install_path' => __DIR__ . '/../symfony/flex',
+            'aliases' => array(),
+            'reference' => 'ab0453b16029e131c112df1a76e59eb2a47e1f67',
+            'dev_requirement' => false,
+        ),
+        'symfony/polyfill-ctype' => array(
+            'dev_requirement' => false,
+            'replaced' => array(
+                0 => '*',
+            ),
+        ),
+        'symfony/polyfill-iconv' => array(
+            'dev_requirement' => false,
+            'replaced' => array(
+                0 => '*',
+            ),
+        ),
+        'symfony/polyfill-php72' => array(
+            'dev_requirement' => false,
+            'replaced' => array(
+                0 => '*',
+            ),
+        ),
+        'symfony/website-skeleton' => array(
+            'pretty_version' => 'v5.2.99',
+            'version' => '5.2.99.0',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => NULL,
+            'dev_requirement' => false,
+        ),
+    ),
+);
